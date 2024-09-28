af4ee86f631983f59ac2d8786178aec53af06a5e
Eduarda Roberta <dudae0437@gmail.com>
2024-09-28 17:51:40 -0300

    somaAtt

--- Soma.py
+++ Soma.py
@@ -1,9 +1,20 @@
-numeroUm = float(input("digite um numero: "))
-numeroDois = float(input("digite outro numero: "))
+# numeroUm = float(input("digite um numero: "))
+# numeroDois = float(input("digite outro numero: "))
 
-soma = numeroUm + numeroDois
+# soma = numeroUm + numeroDois
 
-print("-"*20)
-print("Resultado")
-print(f"{soma}")
+# print("-"*20)
+# print("Resultado")
+# print(f"{soma}")
+
+lista = []
+
+for i in range(5):
+    numero = int(input("Digite cinco numeros: "))
+    lista.append(numero)
+
+soma = 0
+for i in range(len(lista)):
+    soma = soma + lista[i]
+print(soma)
 
